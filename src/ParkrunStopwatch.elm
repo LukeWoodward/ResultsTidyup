8f5f487b884054e622d84a876981ef8d8e731fd6
Luke Woodward <luke.woodward@talk21.com>
2018-06-03 14:46:02 +0100

    Initial vaguely-working version: can upload data from a single stopwatch.

--- src/ParkrunStopwatch.elm
+++ src/ParkrunStopwatch.elm
@@ -1,7 +1,21 @@
 module ParkrunStopwatch exposing (..)
 
-import Html exposing (program)
-import Stopwatch exposing (Stopwatch)
+import Html exposing (Html, program, div, text, table, tr, td, h1)
+import Html.Attributes exposing (class)
+import Stopwatch exposing (Stopwatch(..), readStopwatchData)
+import TimeHandling exposing (formatTime)
+import Ports exposing (fileDrop)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
 
 
 -- Model, Update, View.
@@ -9,9 +23,85 @@ import Stopwatch exposing (Stopwatch)
 
 type alias Model =
     { stopwatches : List Stopwatch
+    , lastError : Maybe String
     }
 
 
-model : Int
-model =
-    0
+initModel : Model
+initModel =
+    { stopwatches = []
+    , lastError = Nothing
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, Cmd.none )
+
+
+type Msg
+    = FileDropped String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        FileDropped fileText ->
+            case readStopwatchData fileText of
+                Ok stopwatch ->
+                    ( { model
+                        | stopwatches = [ stopwatch ]
+                        , lastError = Nothing
+                      }
+                    , Cmd.none
+                    )
+
+                Err error ->
+                    ( { model | lastError = Just error.message }
+                    , Cmd.none
+                    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    fileDrop FileDropped
+
+
+errorView : Maybe String -> List (Html a)
+errorView maybeString =
+    case maybeString of
+        Just string ->
+            [ div [ class "alert alert-danger" ]
+                [ text ("Unable to read in the stopwatch data: " ++ string) ]
+            ]
+
+        Nothing ->
+            []
+
+
+stopwatchRow : Int -> Int -> Html a
+stopwatchRow index time =
+    tr []
+        [ td [] [ text (toString (index + 1)) ]
+        , td [] [ text (formatTime time) ]
+        ]
+
+
+stopwatchView : List Stopwatch -> List (Html a)
+stopwatchView stopwatches =
+    case stopwatches of
+        [] ->
+            []
+
+        (StopwatchData data) :: _ ->
+            [ table [] (List.indexedMap stopwatchRow data) ]
+
+
+view : Model -> Html Msg
+view model =
+    div
+        []
+        (h1 [] [ text "Parkrun stopwatch comparison/merging" ]
+            :: (errorView model.lastError)
+            ++ (stopwatchView model.stopwatches)
+        )
---
+++ src/Ports.elm
@@ -0,0 +1,4 @@
+port module Ports exposing (..)
+
+
+port fileDrop : (String -> msg) -> Sub msg
