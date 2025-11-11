886d725ff50a67b0bbb79f726c9706182bb9e8a6
Luke Woodward <luke.woodward@talk21.com>
2025-11-11 16:36:05 +0000

    Update uglifyjs command following update of uglifyjs to 3.19.3.

--- elm-build-release.bat
+++ elm-build-release.bat
@@ -8,7 +8,7 @@ rmdir /s /q elm-stuff
 
 REM Compile ResultsTidyup in production mode.
 elm make --optimize src/ResultsTidyup.elm  --output results-tidyup.js
-uglifyjs results-tidyup.js --compress "pure_funcs='F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9',pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output=release\results-tidyup.min.js
+uglifyjs results-tidyup.js --compress "pure_funcs='F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9',pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output release\results-tidyup.min.js
 
 copy /y index-release.html release\index.html
 copy /y styles.css release\
